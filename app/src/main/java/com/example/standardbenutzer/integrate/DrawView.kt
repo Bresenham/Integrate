4adf565025bc4803a31ae071115592f950773735
Bresenham <gaul@hm.edu>
2017-12-23 11:14:32 +0100

    Added lower- & upperBound, onTouchListener and debug Textfield

--- app/src/main/java/com/example/standardbenutzer/integrate/DrawView.kt
+++ app/src/main/java/com/example/standardbenutzer/integrate/DrawView.kt
@@ -5,31 +5,83 @@ import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
 import android.util.AttributeSet
+import android.view.MotionEvent
 import android.view.View
 
 /**
  * Created by Standardbenutzer on 22.12.2017.
  */
 class DrawView : View{
+
     private val paint = Paint()
+    private var prevX = 0
+    private var prevY = 0
+    private var lowerBound = 0
+    private var upperBound = 0
+
     constructor(context:Context) : super(context) {
         paint.color = Color.BLACK
+        paint.isAntiAlias = true
+        paint.strokeWidth = 2f
     }
+
     constructor(context:Context,attr: AttributeSet) : super(context,attr) {
         paint.color = Color.BLACK
+        paint.isAntiAlias = true
+        paint.strokeWidth = 2f
     }
+
     override fun onDraw(canvas: Canvas?) {
-        paint.color = Color.BLACK
+        paint.color = Color.WHITE
         canvas?.drawRect(0f,0f,canvas?.width!!.toFloat(),canvas?.height!!.toFloat(),paint)
         val xPoints : MutableList<Int> = mutableListOf()
         val yPoints : MutableList<Int> = mutableListOf()
-        for(i in 0..50){
-            xPoints.add(i+canvas?.width!!.div(2))
-            yPoints.add(Math.exp(i.toDouble()).toInt())
+        for(i in 0..canvas?.width!!){
+            xPoints.add(i)
+            yPoints.add(Math.exp(i.toDouble().div(100)).toInt())
         }
-        paint.color = Color.GREEN
+        paint.color = Color.BLACK
         for(i in 0 until xPoints.size-2){
-            canvas?.drawLine(xPoints[i].toFloat(),canvas?.height - yPoints[i].toFloat(),xPoints[i+1].toFloat(),canvas?.height - yPoints[i+1].toFloat(),paint)
+            val startX = xPoints[i].toFloat()
+            val startY = canvas?.height - yPoints[i].toFloat()
+            val endX = xPoints[i+1].toFloat()
+            val endY = canvas?.height - yPoints[i+1].toFloat()
+            if(startX in upperBound..lowerBound && endX in upperBound..lowerBound){
+                paint.color = Color.RED
+                canvas?.drawRect(startX,startY,endX,canvas?.height.toFloat(),paint)
+            } else {
+                paint.color = Color.BLACK
+                canvas?.drawLine(startX,startY,endX,endY, paint)
+            }
         }
+        drawBounds(canvas)
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val x = event?.x
+        val y = event?.y
+
+        val dX = x?.minus(prevX)
+        val dY = y?.minus(prevY)
+        if(dX!! > 0){
+            lowerBound = x.toInt()
+        } else{
+            upperBound = x.toInt()
+        }
+
+        prevX = x!!.toInt()
+        prevY = y!!.toInt()
+        this.invalidate()
+        return true
+    }
+
+    private fun drawBounds(canvas : Canvas?){
+        paint.color = Color.BLUE
+        canvas?.drawLine(lowerBound.toFloat(),canvas?.height.toFloat(),lowerBound.toFloat(),0.toFloat(),paint)
+        canvas?.drawLine(upperBound.toFloat(),canvas?.height.toFloat(),upperBound.toFloat(),0.toFloat(),paint)
+    }
+
+    fun getText() : String {
+        return "LowerBound: {$lowerBound} | UpperBound: {$upperBound}"
     }
 }
--- app/src/main/java/com/example/standardbenutzer/integrate/MainActivity.kt
+++ app/src/main/java/com/example/standardbenutzer/integrate/MainActivity.kt
@@ -3,10 +3,12 @@ package com.example.standardbenutzer.integrate
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
+import android.text.Editable
 import android.view.Menu
 import android.view.MenuItem
 
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,10 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
-
         fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+            editText.setText(drawView.getText())
         }
     }
 
